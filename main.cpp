6fd61a7fe12a92b45ceb26c285f675e1810fa5f7
대현 <rkdeown10@naver.com>
2023-12-11 13:54:43 +0900

    feature last

--- main.cpp
+++ main.cpp
@@ -17,18 +17,29 @@ int status = 0;
 
 sf::Font font;
 
+//텍스쳐 관리
+
+//학부생
 sf::Texture texture;
-sf::Texture testTexture;
-
-
+//대학원생
 sf::Texture texture2;
-sf::Texture testTexture2;
-
+//박사생
 sf::Texture texture3;
-sf::Texture testTexture3;
-
 sf::Texture texture4;
-sf::Texture testTexture4;
+
+//입학 버튼
+sf::Texture entranceTexture;
+sf::Texture coinTexture;
+sf::Texture daysTexture;
+sf::Texture doublemajorTexture;
+sf::Texture earlygraduationTexture;
+sf::Texture entrancenumberTexture;
+sf::Texture exitTexture;
+sf::Texture personnelincreaseTexture;
+sf::Texture speedofstudyTexture;
+sf::Texture storeTexture;
+sf::Texture transferlevelTexture;
+
 
 class Boo{
     public:
@@ -71,10 +82,9 @@ class Boo{
             text.setCharacterSize(20);
             text.setFillColor(Color::White);
             text.setOutlineThickness(2);
-            text.setPosition(this->posX+25, this->posY-25);
+            text.setPosition(this->posX+25.0f, this->posY-25.0f);
             this->sprite.setPosition(this->posX, this->posY);
             this->sprite.setScale(0.5, 0.5);
-
         }
         
         // x y가 sprite 범위 안에 있는지 확인하는 함수
@@ -94,7 +104,7 @@ class Boo{
             this->posX = x;
             this->posY = y;
             this->sprite.setPosition(this->posX, this->posY);
-            this->text.setPosition(this->posX+25, this->posY-25);
+            this->text.setPosition(this->posX+25.0f, this->posY-25.0f);
         }
         // 충돌하는지 확인
         bool isCollide(Boo boo){
@@ -154,142 +164,177 @@ sf::Time timeToDisplay = sf::seconds(3.0f);
 RenderWindow window(VideoMode(nX, nY), "Boo Grow Game", sf::Style::Titlebar | sf::Style::Close);
 window.setFramerateLimit(240);
 
-
 //텍스쳐 관리
-//학부생
+
+
 texture.loadFromFile("./image/boo.png");
 texture.setSmooth(true);
-//대학원생
 texture2.loadFromFile("./image/boo2.png");
 texture2.setSmooth(true);
-//박사생
 texture3.loadFromFile("./image/boo3.png");
 texture3.setSmooth(true);
-//교수
 texture4.loadFromFile("./image/boo4.png");
 texture4.setSmooth(true);
+entranceTexture.loadFromFile("./image/entrance.png");
+entranceTexture.setSmooth(true);
+coinTexture.loadFromFile("./image/coin.png");
+coinTexture.setSmooth(true);
+daysTexture.loadFromFile("./image/days.png");
+daysTexture.setSmooth(true);
+doublemajorTexture.loadFromFile("./image/doublemajor.png");
+doublemajorTexture.setSmooth(true);
+earlygraduationTexture.loadFromFile("./image/earlygraduation.png");
+earlygraduationTexture.setSmooth(true);
+entrancenumberTexture.loadFromFile("./image/entrancenumber.png");
+entrancenumberTexture.setSmooth(true);
+exitTexture.loadFromFile("./image/exit.png");
+exitTexture.setSmooth(true);
+personnelincreaseTexture.loadFromFile("./image/personnelincrease.png");
+personnelincreaseTexture.setSmooth(true);
+speedofstudyTexture.loadFromFile("./image/speedofstudy.png");
+speedofstudyTexture.setSmooth(true);
+storeTexture.loadFromFile("./image/store.png");
+storeTexture.setSmooth(true);
+transferlevelTexture.loadFromFile("./image/transferlevel.png");
+transferlevelTexture.setSmooth(true);
+
+
+//UI------------------------------------------
 
 //상단바
 sf::RectangleShape topbar(sf::Vector2f(1000.0f, 100.0f));
     topbar.setFillColor(sf::Color(236, 150, 68));
     topbar.setPosition(0.0f, 0.0f);
+
+sf::RectangleShape coin(sf::Vector2f(500.0f, 80.0f));
+    coin.setTexture(&coinTexture);
+    coin.setPosition(0.0f, 0.0f);
+
+Text text;
+text.setFont(font);
+text.setString(to_string(money));
+text.setCharacterSize(30);
+text.setFillColor(Color::White);
+
+//text 테두리 
+text.setOutlineThickness(3);
+text.setPosition(20, 20);
+
+sf::RectangleShape days(sf::Vector2f(500.0f, 80.0f));
+    days.setTexture(&daysTexture);
+    days.setPosition(500.0f, 0.0f);
+
+//text 테두리
+Text text2;
+text2.setFont(font);
+text2.setString(to_string(time_passed));
+text2.setCharacterSize(30);
+text2.setFillColor(Color::White);
+text2.setOutlineThickness(3);
+text2.setPosition(700, 25);
+
+Text text3;
+text3.setFont(font);
+text3.setString(to_string(maxBoo));
+text3.setCharacterSize(50);
+text3.setFillColor(Color::White);
+text3.setOutlineThickness(3);
+text3.setPosition(700, 75);
+
 //하단바
 sf::RectangleShape bottombar(sf::Vector2f(1000.0f, 100.0f));
     bottombar.setFillColor(sf::Color(236, 150, 68));
     bottombar.setPosition(0.0f, 700.0f);
+
 // 배경
 sf::Texture bg;
-bg.loadFromFile("./image/bg2.png");
+bg.loadFromFile("./image/bg.png");
 sf::Sprite background(bg);
 background.setScale(0.3, 0.3);
 background.setPosition(0, 80);
 
-sf::Texture bg2;
-bg2.loadFromFile("./image/bg2.png");
-sf::Sprite background2(bg2);
-background2.setScale(3, 3);
-background2.setPosition(0, 80);
-
 
 // 입학하기 버튼
- sf::RectangleShape button(sf::Vector2f(200.0f, 50.0f));
-    button.setFillColor(sf::Color::Green);
+ sf::RectangleShape button(sf::Vector2f(200.0f, 200.0f));
+    button.setScale(0.5, 0.5);
     button.setPosition(0.0f, 700.0f);
+    button.setTexture(&entranceTexture);
+
 // 입학하기 텍스트
     sf::Text buttonText;
     buttonText.setFont(font);
-    buttonText.setString("Fresh man +1");
+    buttonText.setString("Fresh man +1 price : 1000");
     buttonText.setCharacterSize(20);
     buttonText.setFillColor(Color::White);
     buttonText.setOutlineThickness(2);
-    buttonText.setPosition(25, 710);
+    buttonText.setPosition(100, 725);
+
+
 // 증원하기 버튼
-    sf::RectangleShape button2(sf::Vector2f(200.0f, 50.0f));
-        button2.setFillColor(sf::Color::Red);
-        button2.setPosition(200.0f, 700.0f);
+    sf::RectangleShape button2(sf::Vector2f(800.0f, 100.0f));
+        button2.setTexture(&personnelincreaseTexture);
+        button2.setPosition(100.0f, 150.0f);
+
 // 증원하기 텍스트
     sf::Text button2Text;
     button2Text.setFont(font);
-    button2Text.setString("maxBoo Lv."+to_string(maxBoo));
+    button2Text.setString("maxBoo Lv."+to_string(maxBoo)+" price"+to_string(1000));
     button2Text.setCharacterSize(20);
     button2Text.setFillColor(Color::White);
     button2Text.setOutlineThickness(2);
-    button2Text.setPosition(225, 710);
+    button2Text.setPosition(450.0f, 175.0f);
 // 공부하기 버튼
-    sf::RectangleShape button3(sf::Vector2f(200.0f, 50.0f));
-        button3.setFillColor(sf::Color::Blue);
-        button3.setPosition(400.0f, 700.0f);
+    sf::RectangleShape button3(sf::Vector2f(800.0f, 100.0f));
+        button3.setTexture(&speedofstudyTexture);
+        button3.setPosition(100.0f, 250.0f);
 // 공부하기 텍스트
     sf::Text button3Text;
     button3Text.setFont(font);
-    button3Text.setString("studyLevel Lv."+to_string(studyLevel));
+    button3Text.setString("studyLevel Lv."+to_string(studyLevel) + " price : " + to_string(1000));
     button3Text.setCharacterSize(20);
     button3Text.setFillColor(Color::White);
     button3Text.setOutlineThickness(2);
-    button3Text.setPosition(425, 710);
+    button3Text.setPosition(450.0f, 275.0f);
 
 // 이중전공 버튼
-    sf::RectangleShape button4(sf::Vector2f(200.0f, 50.0f));
-        button4.setFillColor(sf::Color::Yellow);
-        button4.setPosition(0.0f, 750.0f);
+    sf::RectangleShape button4(sf::Vector2f(800.0f, 100.0f));
+        button4.setTexture(&doublemajorTexture);
+        button4.setPosition(100.0f, 350.0f);
 //이중전공 텍스트
     sf::Text button4Text;
     button4Text.setFont(font);
-    button4Text.setString("doubleLevel Lv."+to_string(doubleLevel));
+    button4Text.setString("doubleLevel Lv."+to_string(doubleLevel) + " price : " + to_string(10000));
     button4Text.setCharacterSize(20);
     button4Text.setFillColor(Color::White);
     button4Text.setOutlineThickness(2);
-    button4Text.setPosition(25, 760);
+    button4Text.setPosition(450.0f, 375.0f);
 // 조기졸업 버튼
-    sf::RectangleShape button5(sf::Vector2f(200.0f, 50.0f));
-        button5.setFillColor(sf::Color::Magenta);
-        button5.setPosition(200.0f, 750.0f);
+    sf::RectangleShape button5(sf::Vector2f(800.0f, 100.0f));
+        button5.setTexture(&earlygraduationTexture);
+        button5.setPosition(100.0f, 450.0f);
 // 조기졸업 텍스트
     sf::Text button5Text;
-    button5Text.setFont(font);
-    button5Text.setString("fastLevel Lv."+to_string(fastLevel));
-    button5Text.setCharacterSize(20);
-    button5Text.setFillColor(Color::White);
-    button5Text.setOutlineThickness(2);
-    button5Text.setPosition(225, 760);
-// 시험 버튼
-    sf::RectangleShape button6(sf::Vector2f(200.0f, 50.0f));
-        button6.setFillColor(sf::Color::Cyan);
-        button6.setPosition(400.0f, 750.0f);
-// 시험 텍스트
+        button5Text.setFont(font);
+        button5Text.setString("fastLevel Lv."+to_string(fastLevel) + "/10" + " price : " + to_string(100000));
+        button5Text.setCharacterSize(20);
+        button5Text.setFillColor(Color::White);
+        button5Text.setOutlineThickness(2);
+        //button5 중간에 위치
+        button5Text.setPosition(450.0f, 475.0f);
+
+// 상점 버튼
+    sf::RectangleShape button6(sf::Vector2f(100.0f, 100.0f));
+        button6.setTexture(&storeTexture);
+        button6.setPosition(600.0f, 700.0f);
+// 상점 텍스트
     sf::Text button6Text;
-    button6Text.setFont(font);
-    button6Text.setString("go to test");
-    button6Text.setCharacterSize(20);
-    button6Text.setFillColor(Color::White);
-    button6Text.setOutlineThickness(2);
-    button6Text.setPosition(425, 760);
+    // button6Text.setFont(font);
+    // button6Text.setString("Level "+to_string(status));
+    // button6Text.setCharacterSize(20);
+    // button6Text.setFillColor(Color::White);
+    // button6Text.setOutlineThickness(2);
+    // // button6의 위치의 중간에 위치
 
-// 시험 버튼들 1
-    sf::RectangleShape button7(sf::Vector2f(200.0f, 50.0f));
-        button7.setFillColor(sf::Color::Cyan);
-        button7.setPosition(50.0f, 200.0f);
-// 시험 버튼들 2
-    sf::RectangleShape button8(sf::Vector2f(200.0f, 50.0f));
-        button8.setFillColor(sf::Color::Cyan);
-        button8.setPosition(300.0f, 200.0f);
-// 시험 버튼들 3
-    sf::RectangleShape button9(sf::Vector2f(200.0f, 50.0f));
-        button9.setFillColor(sf::Color::Cyan);
-        button9.setPosition(50.0f, 400.0f);
-// 시험 버튼들 4
-    sf::RectangleShape button10(sf::Vector2f(200.0f, 50.0f));
-        button10.setFillColor(sf::Color::Cyan);
-        button10.setPosition(300.0f, 400.0f);
-
-Text text;
-text.setFont(font);
-text.setString(to_string(money) + " won" + " " + to_string(time_passed) + " sec");
-text.setCharacterSize(30);
-text.setFillColor(Color::White);
-//text 테두리 
-text.setOutlineThickness(3);
-text.setPosition(0, 0);
 
 // Boo 객체를 생성한다.
 vector<Boo> boos;
@@ -340,9 +385,12 @@ if (Mouse::isButtonPressed(Mouse::Left)) {
                     }
                 }
                 window.draw(boos[i].sprite);
-                window.draw(boos[i].text);
+                window.draw(boos[i].text);  
+                window.draw(coin);
+                window.draw(days);
                 window.draw(text);
-            
+                window.draw(text2);
+                window.draw(text3);
                 window.draw(button6);
                 window.draw(button6Text);
                 window.draw(button);
@@ -353,18 +401,17 @@ if (Mouse::isButtonPressed(Mouse::Left)) {
         }
             for (Boo boo2 : boos) {
                 if (boos[i].isCollide(boo2) && boos[i].uid != boo2.uid && boos[i].level == boo2.level) {
-                    
                     int level = 0;
 
                     //조기졸업 확률코드 추가
                     int randNum = rand() % 100;
-                    if (boo2.level == 3 &&randNum < 10 * fastLevel) {
+                    if (boo2.level == 3 && randNum < 10 * fastLevel) {
                         level = 5;
                     } else {
                         level = boo2.level + 1;
                     }
 
-                    Boo tempBoo(++uid, level, boos[i].power*3, boos[i].posX, boos[i].posY, boos[i].money*2, font);
+                    Boo tempBoo(++uid, level, boos[i].power*3, boos[i].posX, boos[i].posY, boos[i].money, font);
 
                     
                     //boo, boo2 삭제
@@ -393,9 +440,39 @@ if (e.type == sf::Event::MouseButtonPressed) {
                 goto end;
             }
 
+            if (maxBoo <= 10) {
+                if (money < 1000) {
+                    cout << "돈이 부족합니다" << endl;
+                    goto end;
+                }
+                buttonText.setString("Fresh man +1 price : "+to_string(1000));
+                money -= 1000;
+            } else if (maxBoo <= 25) {
+                if(money < 10000) {
+                    cout << "돈이 부족합니다" << endl;
+                    goto end;
+                }
+                buttonText.setString("Fresh man +1 price : "+to_string(10000));
+                money -= 10000;
+            } else if (maxBoo <= 50) {
+                if(money < 50000) {
+                    cout << "돈이 부족합니다" << endl;
+                    goto end;
+                }
+                buttonText.setString("Fresh man +1 price : "+to_string(50000));
+                money -= 50000;
+            } else if (maxBoo <= 75) {
+                if(money < 100000) {
+                    cout << "돈이 부족합니다" << endl;
+                    goto end;
+                }
+                buttonText.setString("Fresh man +1 price : "+to_string(100000));
+                money -= 100000;
+            }
+
             //화면 밖으로 빠져나오지 않게 범위안에 랜덤 생성
-            int randx = rand() % 1000;
-            int randy = rand() % 500;
+            float randx = rand() % 1000;
+            float randy = rand() % 500;
             if (randx > 1000) {
                 randx = 900;
             } else if (randx < 100) {
@@ -418,46 +495,70 @@ if (e.type == sf::Event::MouseButtonPressed) {
                 window.draw(boo2.text);
             }
             window.display();
-
             std::cout << "Button Clicked!" << std::endl;
-            money -= 100;
-        }
-        
-    else if (button2.getGlobalBounds().contains(mousePos) && money >= 10000) {
-        maxBoo += 1;
-        money -= 10000;
-        button2Text.setString("maxBoo Lv."+to_string(maxBoo));
-    } else if (button3.getGlobalBounds().contains(mousePos)) {
-        //:todo 강화 가격 증가
-        studyLevel += 1;
-        button3Text.setString("studyLevel Lv."+to_string(studyLevel));
-    } else if (button4.getGlobalBounds().contains(mousePos)) {
-        //:todo 이중 가격 증가
-        doubleLevel += 1;
-        button4Text.setString("doubleLevel Lv."+to_string(doubleLevel));
-    } else if (button5.getGlobalBounds().contains(mousePos)) {
-        //:todo 조기 가격 증가
-        fastLevel += 1;
-        button5Text.setString("fastLevel Lv."+to_string(fastLevel));
-    } else if (button6.getGlobalBounds().contains(mousePos)) {
+
+        } else if (button6.getGlobalBounds().contains(mousePos)) {
         if (status == 0) {
             status = 1;
             button6Text.setString("back");
         } else {
-            (status = 0);
+            status = 0;
             button6Text.setString("goto test");
         }
-    } else if (button7.getGlobalBounds().contains(mousePos)) {
-        
-    } else if (button8.getGlobalBounds().contains(mousePos)) {
-
-    } else if (button9.getGlobalBounds().contains(mousePos)) {
-
-    } else if (button10.getGlobalBounds().contains(mousePos)) {
-    } 
-    else {
+    } else {
         std::cout << "빈곳을 클릭함" << std::endl;
     }
+
+    if (status == 1) {
+        if (button2.getGlobalBounds().contains(mousePos)) {
+         if (maxBoo < 10 && money >= 1000) {
+            money -= 1000;
+            maxBoo += 1;
+            button2Text.setString("maxBoo Lv."+to_string(maxBoo)+" price"+to_string(1000));
+         } else if (maxBoo<25 && money >= 10000) {
+            money -= 10000;
+            maxBoo += 1;
+            button2Text.setString("maxBoo Lv."+to_string(maxBoo)+" price"+to_string(10000));
+         } else if (maxBoo<50 && money >= 100000) {
+            money -= 100000;
+            maxBoo += 1;
+            button2Text.setString("maxBoo Lv."+to_string(maxBoo)+" price"+to_string(100000));
+         } else if (maxBoo<75 && money >= 300000) {
+            money -= 300000;
+            maxBoo += 1;
+            button2Text.setString("maxBoo Lv."+to_string(maxBoo)+" price"+to_string(300000));
+         } else if (maxBoo>75 && money>= 500000) {
+            money -= 500000;
+            maxBoo += 1;
+            button2Text.setString("maxBoo Lv."+to_string(maxBoo)+" price"+to_string(500000));
+         }
+    } else if (button3.getGlobalBounds().contains(mousePos)) {
+        //:todo 강화 가격 증가
+        if (money >= studyLevel*1000){
+        money -= studyLevel*1000;
+        studyLevel += 1;
+        button3Text.setString("studyLevel Lv."+to_string(studyLevel) + " price : " + to_string(studyLevel*1000));
+
+        }
+    } else if (button4.getGlobalBounds().contains(mousePos)) {
+        //:todo 이중 가격 증가
+        if (money>= doubleLevel*10000) {
+        money -= doubleLevel*10000;
+        doubleLevel += 1;
+        button4Text.setString("doubleLevel Lv."+to_string(doubleLevel) + " price : " + to_string(doubleLevel*10000));
+
+        }
+    } else if (button5.getGlobalBounds().contains(mousePos) && fastLevel <= 10) {
+        //:todo 조기 가격 증가
+        if (money >= 100000 * fastLevel) {
+        money -= 100000 * fastLevel;
+        fastLevel += 1;
+        button5Text.setString("fastLevel Lv."+to_string(fastLevel)+"/10" + " price : " + to_string(100000 * fastLevel));
+
+        }
+    }
+    }
+
     }
 }
 
@@ -471,7 +572,9 @@ for (Boo boo : boos) {
     }
 }
 time_passed = time_end - time_start;
-text.setString(to_string(money) + " won" + " " + to_string(time_passed) + " sec " + to_string(boos.size()) + "/" + to_string(maxBoo) + "person");
+text.setString(to_string(money));
+text2.setString(to_string(time_passed));
+text3.setString(to_string(boos.size())+"/"+to_string(maxBoo) + " person");
 //
 
 if (status == 0){
@@ -484,8 +587,11 @@ for (Boo boo : boos) {
     window.draw(boo.sprite);
     window.draw(boo.text);
 }
-
+window.draw(coin);
+window.draw(days);
 window.draw(text);
+window.draw(text2);
+window.draw(text3);
 window.draw(button);
 window.draw(buttonText);
 window.draw(button6);
@@ -500,9 +606,11 @@ window.display();
     window.draw(background);
     window.draw(bottombar);
     window.draw(button6);
-
+    window.draw(coin);
+    window.draw(days);
     window.draw(text);
-    
+    window.draw(text2);
+    window.draw(text3);
     window.draw(button2);
     window.draw(button3);
     window.draw(button4);
